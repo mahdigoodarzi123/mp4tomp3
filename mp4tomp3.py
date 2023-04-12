f4ca9e98bca576e758ad02c2e54dfdab35f988ef
mahdigoodarzi123 <erfangoodarzi123@gmail.com>
2023-04-12 00:50:39 -0700

    using pydub module bc of its better performance

--- mp4tomp3.py
+++ mp4tomp3.py
@@ -1,11 +1,11 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
+# from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 
-def converter(filename):
-        with VideoFileClip(filename) as video:
-                audio = video.audio
-                audio.write_audiofile("sound.mp3")
+# def converter(filename):
+#         with VideoFileClip(filename) as video:
+#                 audio = video.audio
+#                 audio.write_audiofile("sound.mp3")
 
 
 # import os
@@ -15,3 +15,11 @@ def converter(filename):
 
 # Example usage
 # rename_file('vid.mp4', 'sound.mp3')
+
+
+from pydub import AudioSegment
+
+
+def converter(filename):
+        sound = AudioSegment.from_file(filename, format="mp4")
+        sound.export("sound.mp3", format="mp3")
